28485279c8f66a3d54a71f4ac26ba4d370c557e3
priyanshu prajapati <princek57350@gmail.com>
2026-09-03 12:25:17 +0530

    ✅ Second Largest | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-03

--- gfg/easy/second-largest3735/solution.java
+++ gfg/easy/second-largest3735/solution.java
@@ -1,19 +1,45 @@
 class Solution {
     public int getSecondLargest(int[] arr) {
         // code here
+        // int first = arr[0];
+        // for(int i = 0; i <arr.length; i++){
+        //         if(first<arr[i]) first=arr[i];
+        // }
+        
+        // int second = 0;
+        
+        //  for(int i = 0; i <arr.length; i++){
+        //      if(arr[i] == first) continue;
+        //      if(second<arr[i]) second=arr[i];
+        // }
+        
+        // return second ==0? -1:second;
+        
+        
+        // int first = arr[0];
+        // int second = arr[0];
+        // for(int i = 0 ; i<arr.length; i++){
+        //     if (first<arr[i] || second<arr[i]){
+        //         if(arr[i] == first) continue;
+        //     }
+        //     if(second <arr[i]) second = arr[i];
+        // }
+        //  return second ==0? -1:second;
+        
         int first = arr[0];
-        for(int i = 0; i <arr.length; i++){
-                if(first<arr[i]) first=arr[i];
+        int second = Integer.MIN_VALUE;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > first) {
+                second = first;
+                first = arr[i];
+            }
+            else if (arr[i] > second && arr[i] != first) {
+                second = arr[i];
+            }
         }
-        
-        int second = 0;
-        
-         for(int i = 0; i <arr.length; i++){
-             if(arr[i] == first) continue;
-             if(second<arr[i]) second=arr[i];
-        }
-        
-        return second ==0? -1:second;
-        
+
+        return second == Integer.MIN_VALUE ? -1 : second;
     }
 }
