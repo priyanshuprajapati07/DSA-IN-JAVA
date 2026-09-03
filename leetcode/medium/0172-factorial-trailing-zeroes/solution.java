d410df15854a3d1f3c773b3a2231f368319a3929
priyanshu prajapati <princek57350@gmail.com>
2026-09-03 09:53:24 +0530

    ✅ Factorial Trailing Zeroes | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 42.5 MB | 2026-09-03

--- leetcode/medium/0172-factorial-trailing-zeroes/solution.java
+++ leetcode/medium/0172-factorial-trailing-zeroes/solution.java
@@ -1,17 +1,24 @@
 class Solution {
     public int trailingZeroes(int n) {
-        int fact = 1;
-        for (int i = 1; i<=n; i++){
-            fact *=i;
-        }
-        int count = 0;
-        while(fact<=n){
-            int last = fact%10;
-            fact = fact/10;
-            if(last == 0){
-                count++;
-            }
-        }
-       return count++;
+    //     int fact = 1;
+    //     for (int i = 1; i<=n; i++){
+    //         fact *=i;
+    //     }
+    //     int count = 0;
+    //     while(fact!=0){
+    //         int last = fact%10;
+    //         fact = fact/10;
+    //         if(last == 0){
+    //             count++;
+    //         }
+    //     }
+    //    return count++;
+
+    int ans = 0;
+    while(n>=5){
+        n = n /5;
+        ans +=n;
+    }
+    return ans;
     }
 }
