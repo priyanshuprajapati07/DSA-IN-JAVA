ba61a36a62aa3797c71a8784601ef8051aba08c8
priyanshu prajapati <princek57350@gmail.com>
2026-09-03 10:57:34 +0530

    ✅ Array Traversal | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-03

---
+++ gfg/basic/array-traversal/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static void arrayTraversal(int[] arr) {
+        // Code here
+        for(int i = 0; i<arr.length; i++){
+            // return arr + " ";
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
