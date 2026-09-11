ee4b9d3e1961710024de17b6b68abfcd9704ff4c
priyanshu prajapati <princek57350@gmail.com>
2026-09-11 14:04:37 +0530

    ✅ Segregate 0s and 1s | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-11

--- gfg/easy/segregate-0s-and-1s5106/solution.java
+++ gfg/easy/segregate-0s-and-1s5106/solution.java
@@ -1,17 +1,18 @@
 class Solution {
     void segregate0and1(int[] arr) {
         // code here
-        int left = 0;
-        int right = arr.length-1;
+        // int left = 0;
+        // int right = arr.length-1;
         
-        while(left<right){
-            if(arr[left]==1 && arr[right]==0){
-                //swap
-                arr[left] =0;
-                arr[right] = 1;
-            }
-            if(arr[left]==0) left++;
-            if(arr[right]==1) right--; 
-        }
+        // while(left<right){
+        //     if(arr[left]==1 && arr[right]==0){
+        //         //swap
+        //         arr[left] =0;
+        //         arr[right] = 1;
+        //     }
+        //     if(arr[left]==0) left++;
+        //     if(arr[right]==1) right--; 
+        // }
+        Arrays.sort(arr);
     }
 }
