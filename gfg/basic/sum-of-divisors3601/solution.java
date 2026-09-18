5f3c7f7341d9cad360d3b587479ee3269c78c9cf
priyanshu prajapati <princek57350@gmail.com>
2026-09-18 14:24:41 +0530

    ✅ Sum of Divisors | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-18

---
+++ gfg/basic/sum-of-divisors3601/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    static long divSum(long n) {
+        // code here
+        int sum = 0;
+        for(int i = 1; i<n; i++){
+            if(n%i == 0) sum+=i;
+        }
+        return sum;
+    }
+}
