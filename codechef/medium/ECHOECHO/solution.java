5ad952960698bc014ec5fba2aa6b6e500342da17
priyanshu prajapati <princek57350@gmail.com>
2026-09-23 21:41:50 +0530

    ✅ ECHOECHO | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/ECHOECHO/solution.java
+++ codechef/medium/ECHOECHO/solution.java
@@ -7,9 +7,9 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-	Scanner sc = new Scanner(System.in);
-	String s = sc.next();
-	if(s.charAt(0) == s.charAt(2) && s.charAt(1) == s.charAt(3)){
+	Scanner sc=new Scanner(System.in);
+	String s=sc.next();
+	if(s.charAt(0)==s.charAt(2)&&s.charAt(1)==s.charAt(3)){
 	    System.out.println("yes");
 	}else{
 	    System.out.println("no");
