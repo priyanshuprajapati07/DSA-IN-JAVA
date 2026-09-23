64747bd2f37599e64e9258c246692572ce8aef9a
priyanshu prajapati <princek57350@gmail.com>
2026-09-23 21:10:55 +0530

    ✅ PGNATK | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/PGNATK/solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		if(sc.hasNextInt()){
+		    int t = sc.nextInt();
+		    
+		    while (t-- > 0){
+		        int n = sc.nextInt();
+		        int k = sc.nextInt();
+		        int totalMinutes = n + (n-1)/(k-1);
+		        
+		        System.out.println(totalMinutes);
+		    }
+		}
+		sc.close();
+
+	}
+}
